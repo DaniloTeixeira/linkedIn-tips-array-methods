bbeeadcac869c8b94ba236a3836a59ee3f6408fb
Danilo Rodrigues Teixeira <danilo.rt@outlook.com>
2023-01-27 18:38:06 -0300

    criando o metodo join e inserindo imagens

---
+++ array-methods-js/2.join.js
@@ -0,0 +1,8 @@
+/* A função join() do JavaScript é usada para juntar todos os elementos de um array em uma string, separados por um delimitador especificado. Ela retorna uma nova string com os elementos juntos. */
+
+const cars = [ "Ferrari", "Porsche", "Mercedez" ];
+const carList = cars.join(", ");
+
+console.log(carList); // "Ferrari, Porsche, Mercedes"
+
+/* Neste exemplo, a função join() é usada para juntar todos os elementos do array cars em uma string, separando-os com uma vírgula e um espaço. */
