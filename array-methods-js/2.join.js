323555f57b6fee566a0fd527acde3919897caf73
Danilo Rodrigues Teixeira <danilo.rt@outlook.com>
2023-01-27 23:29:37 -0300

    Correção de nome do item do array cars

--- array-methods-js/2.join.js
+++ array-methods-js/2.join.js
@@ -1,6 +1,6 @@
 /* A função join() do JavaScript é usada para juntar todos os elementos de um array em uma string, separados por um delimitador especificado. Ela retorna uma nova string com os elementos juntos. */
 
-const cars = [ "Ferrari", "Porsche", "Mercedez" ];
+const cars = [ "Ferrari", "Porsche", "Mercedes" ];
 const carList = cars.join(", ");
 
 console.log(carList); // "Ferrari, Porsche, Mercedes"
