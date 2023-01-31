c20dd68945416c07895ca66d6c04113c31918b45
Danilo Rodrigues Teixeira <danilo.rt@outlook.com>
2023-01-31 12:33:08 -0300

    Método push

---
+++ array-methods-js/3.push.js
@@ -0,0 +1,15 @@
+/* A função push é usada para adicionar um ou mais elementos ao final de um array. Ela é uma das operações mais comuns realizadas em arrays.
+
+A sintaxe da função push é a seguinte:
+
+array.push(item1, item2, ..., itemX);
+
+Onde array é o nome do array que você deseja adicionar elementos, e item1, item2, ..., itemX são os elementos que você deseja adicionar ao final do array. */
+
+const fruits = [ 'apple', 'banana' ];
+
+fruits.push('orange', 'grapes');
+
+console.log(fruits); // ["apple", "banana", "orange", "grapes"]
+
+/* Neste exemplo, primeiro criamos um array fruits com dois elementos, apple e banana. Em seguida, usamos a função push para adicionar mais dois elementos, orange e grapes, ao final do array fruits. */
