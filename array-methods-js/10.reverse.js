e819552f1a0b35c227849049105eacbb333b5717
Danilo Rodrigues Teixeira <danilo.rt@outlook.com>
2023-02-10 11:14:57 -0300

    Método reverse

---
+++ array-methods-js/10.reverse.js
@@ -0,0 +1,15 @@
+/*
+ O método reverse é um método que inverte a ordem dos elementos de um array. 
+ Ele modifica o array original e retorna o mesmo array, com os elementos agora na ordem inversa.
+*/
+
+const crazyNumbers = [ 1, 2, 3, 4, 5 ];
+
+crazyNumbers.reverse();
+
+console.log(crazyNumbers); // Output: [5, 4, 3, 2, 1]
+
+/* 
+ No exemplo acima, invertemos a ordem dos elementos do array "números". 
+ Observe que o array original é modificado.
+*/
